9e993d4a16bd4379d45a455a2bfde35ccc2a39d0
Comamoca <comamoca.dev@gmail.com>
2024-04-03 00:05:15 +0900

    :tada: (All) Add source code.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,46 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import solid from "vite-plugin-solid";
 
+const AppName = "mojicounter";
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // devOptions: {
+      //   enabled: true,
+      // },
+      manifest: {
+        name: AppName,
+        short_name: AppName,
+        description: "シンプルな文字数カウンター",
+
+        icons: [
+          {
+            src: "./public/favicon.svg",
+            type: "image/svg+xml",
+            sizes: "192x192",
+          },
+          {
+            src: "./public/favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+          {
+            src: "./public/favicon.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+        start_url: "index.html",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        lang: "ja",
+      },
+    }),
+  ],
 });
